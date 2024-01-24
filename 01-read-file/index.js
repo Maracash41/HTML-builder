b9848e3393d228ac1aa52688c7ec893973f03387
Maracash41 <9531945@gmail.com>
2024-01-24 18:22:55 +0300

    fix: 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const { stdout } = process;
-fs.readFile(path.join(__dirname, 'text.txt'), (error, data) => {
-  if (error) console.error(error.message);
-  stdout.write(`${data.toString()}`);
-})
+const stream = fs.ReadStream(path.join(__dirname, 'text.txt'));
+
+stream.on('readable', function () {
+  let data;
+  if (null !== (data = stream.read())) {
+    stdout.write(data.toString('utf-8'));
+  }
+});
+// fs.readFile(path.join(__dirname, 'text.txt'), (error, data) => {
+//   if (error) console.error(error.message);
+//   stdout.write(`${data.toString()}`);
+// })
